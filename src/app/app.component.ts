c675db4061f61ececd569b69e0e80fee9831ef05
Eric Barb <eric.barb@gmail.com>
2019-06-02 20:09:03 -0400

    Google Analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+declare var gtag;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Kids-Game';
+
+  constructor(private router: Router) {
+    const navEndEvents = router.events.pipe(filter(event => event instanceof NavigationEnd));
+    navEndEvents.subscribe((event: NavigationEnd) => {
+      gtag('config', 'UA-141286305-1', { page_path: event.urlAfterRedirects });
+    });
+  }
 }
